c6e114453e563660c7299273e34093a1017e92cc
Ubuntu <ubuntu@ip-172-31-1-11.ec2.internal>
2023-11-13 14:56:33 +0000

    utilizando o flask_smorest

--- app.py
+++ app.py
@@ -1,35 +1,36 @@
 from flask import Flask, request
+from db import items, stores
 
 app = Flask(__name__)
 
-stores = [
-    {
-        "name": "My store",
-        "items": [
-            {
-                "name": "Chair",
-                "price": 15.99
-            }
-            ]
-    }
-    ]
+
     
 
 #buscar lojas
 @app.get("/store")
 def get_stores():
-    return {"stores":stores}
+    return {"stores":list(stores.value())}
+    
+@app.get("/store<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        return {"message": "Store not found"}, 404
+        
 
 #criar lojas
 @app.post("/store")
 def create_store():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items":[]}
-    stores.append(new_store)
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex #f5f564564165ff8684d6
+    new_store = {**store_data, "id": store_id}
+    stores[store_id] = new_store
     return new_store, 201
+    
 
 #criar itens
-@app.post("/store/<string:name/item>")
+@app.post("/item")
 def create_item(name):
     request_data = request.get_json()
     for store in stores:
